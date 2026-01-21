3240b1b7801424817bb1eb0b2e3415ce647b6571
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-01-22 03:22:04 +0600

    add component for render meals card

---
+++ src/components/Meal/MealCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './MealCard.css';
+import type IMeal from '../../../types/meals/meal';
+import Button from '../../UI/Button/Button';
+import { Link } from 'react-router-dom';
+
+interface IMealCard {
+  mealData: IMeal;
+}
+
+const MealCard: React.FC<IMealCard> = ({ mealData }) => {
+  return (
+    <div className="meal-card">
+      <div className="meal-card-info">
+        <p className="meal-time">{mealData.meal_time}</p>
+        <p className="meal-description">{mealData.meal_description}</p>
+      </div>
+
+      <div className="meal-bottom">
+        <p className="meal-calorie">{mealData.meal_calorie} kcal</p>
+
+        <div className="meal-actions">
+          <Link className="meal-edit" to={`meals/${mealData.id}/edit`}/>
+          <Button className="meal-delete" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MealCard;
