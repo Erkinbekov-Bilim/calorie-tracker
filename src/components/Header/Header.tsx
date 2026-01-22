f04f7cd9a208b7c72a35acdc7b5f9c2c39467666
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-01-22 19:38:49 +0600

    changed the tag from a paragraph to a link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <>
-      <header className='header'>
-        <p>Calorie Tracker</p>
+      <header className="header">
+        <Link to={'/'}>Calorie Tracker</Link>
       </header>
     </>
   );
